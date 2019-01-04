9cf86c9c0e23796d77b1265fe9c24b420cb3c061
haneulee <lovesky4294@gmail.com>
2019-01-04 15:50:28 +0900

    add date picker

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Datepicker from "./components/Datepicker";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="w-25 py-5 my-5 mx-auto">
+        <Datepicker label="Birthday" value="2000-08-15" />
+      </div>
+    );
+  }
+}
+
+export default App;
